86660383fd7e039fa3092c32ba55b21845dae03c
LubaKaper <liubovkaper@pursuit.org>
2020-10-02 14:44:44 -0400

    lemonade

---
+++ Pages/largestMultiply3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+
+//Given an array of at least three integers, return the largest product you can get by multiplying three numbers together.
+
+func multipleThree(arr: [Int]) -> Int {
+    var sortedArr = arr.sorted()
+   
+    if sortedArr[sortedArr.count - 1] > 0 && sortedArr[sortedArr.count - 2] > 0 && sortedArr[sortedArr.count - 3] > 0 {
+        
+    }
+    //-3, -2, 0, 1, 2
+return 0
+}
