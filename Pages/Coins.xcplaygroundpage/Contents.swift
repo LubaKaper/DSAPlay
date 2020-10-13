4becda55f7587ee9af0c744f242372009f9cf32f
LubaKaper <liubovkaper@pursuit.org>
2020-10-12 22:00:59 -0400

    swap2Ints

---
+++ Pages/Coins.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//Given coins of value 1, 3, and 6 and a sum, what is the minimum number of coins needed to reach the sum
+
+func coins(coins: [Int], sum: Int) -> Int {
+   
+    
+    return 0
+}
