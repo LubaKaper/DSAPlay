313929ddd99e53c366522b08d5be443d45f8a351
LubaKaper <liubovkaper@pursuit.org>
2020-10-23 19:07:27 -0400

    swap nodes

---
+++ Pages/SwapNodes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//Given the head node of a singly linked list, swap each pair of nodes and return the head. If there is a last odd node leave it in place.
