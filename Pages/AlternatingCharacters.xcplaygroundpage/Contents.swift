369a8957e80f07ffc654c872548db179c69a1956
LubaKaper <liubovkaper@pursuit.org>
2020-11-04 16:52:16 -0500

    x

--- Pages/AlternatingCharacters.xcplaygroundpage/Contents.swift
+++ Pages/AlternatingCharacters.xcplaygroundpage/Contents.swift
@@ -2,6 +2,12 @@
 
 import Foundation
 
+//You are given a string containing characters  and  only. Your task is to change it into a string such that there are no matching adjacent characters. To do this, you are allowed to delete zero or more characters in the string.
+//
+//Your task is to find the minimum number of required deletions.
+//
+//For example, given the string , remove an  at positions  and  to make  in  deletions.
+
 func alternatingCharacters(s: String) -> Int {
     let inputArr = Array(s)
     var newArr = [Character]()
@@ -16,4 +22,4 @@ func alternatingCharacters(s: String) -> Int {
     return newArr.count
 }
 
-print(alternatingCharacters(s: "AAABBB"))
+print(alternatingCharacters(s: "AAABBBAAA"))
