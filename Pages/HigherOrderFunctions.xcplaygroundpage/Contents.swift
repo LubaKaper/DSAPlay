822732134c69d35aaf4a9bb72833abe9a6cca256
LubaKaper <liubovkaper@pursuit.org>
2020-11-24 14:29:00 -0500

    .filter:

--- Pages/HigherOrderFunctions.xcplaygroundpage/Contents.swift
+++ Pages/HigherOrderFunctions.xcplaygroundpage/Contents.swift
@@ -2,7 +2,8 @@
 
 import Foundation
 
-// .map works on a colllections and performs a given transformation on each element and returns a new collection.
+// .map
+// works on a colllections and performs a given transformation on each element and returns a new collection.
 
 let values = [1,2,3,4,5]
 
@@ -15,3 +16,26 @@ print(multiplyByItself)
 
 print(multipyBy2)
 print(transformedValues)
+
+
+//.reduce
+//takes an initial value and a closure that combines the previous result and current value to return a combined value.
+
+let reduce = values.reduce(0, +)
+print(reduce)
+
+
+ // .compactMap
+// returns non-nil values from a given transformation.
+
+let mixedValue = ["true", "1", "0", "false", "false"]
+
+let booleanValues = mixedValue.compactMap{ Bool($0)}
+print(booleanValues)
+
+// .filter
+// takes a closure that operates as a predicate on the elements of a sequence and returns the results of the query.
+
+let values2 = [3, 1, 4, 2]
+let filteredValues = values2.filter { $0 > 2 }
+print(filteredValues)
